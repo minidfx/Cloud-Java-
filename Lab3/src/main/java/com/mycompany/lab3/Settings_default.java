1db48bc833e181f40b7c3b8b6ff3e7401e3971d2
Burgy Benjamin <minidfx@gmail.com>
2015-10-31 13:57:09 +0100

    Save the structure of the settings file.

---
+++ Lab3/src/main/java/com/mycompany/lab3/Settings_default.java
@@ -0,0 +1,17 @@
+package com.mycompany.lab3;
+
+/**
+ *
+ * @author Burgy Benjamin
+ */
+public final class Settings_default
+{
+    public String OpenStack_user = "";
+    public String OpenStack_tenant_name = "";
+    public String OpenStack_service_region = "";
+    public String OpenStack_auth_url = "";
+    public String OpenStack_password = "";
+
+    public String Amazon_accessKey = "";
+    public String Amazon_secretKey = "";
+}
